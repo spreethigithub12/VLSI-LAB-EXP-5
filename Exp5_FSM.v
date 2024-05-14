358451ee1bb536a2ec5c6cbbb21992fb8ae2a462
spreethigithub12 <169068043+spreethigithub12@users.noreply.github.com>
2024-05-14 11:37:22 +0530

    Update Exp5_FSM.v

--- Exp5_FSM.v
+++ Exp5_FSM.v
@@ -1,17 +1,31 @@
-module FSM_moore(clk, rst, x, z);
+module fsm(clk, rst, x, z);
 input clk, rst, x;
 output z;
-reg [2:1] present_state, NEXT_STATE; 
+reg [2:1] present_state, NEXT_STATE;
 parameter S0=2'b00, S1=2'b01, S2=2'b10, S3=2'b11;
-// define the next state combinational circuit
 always@(x,present_state)
 case(present_state)
-
+S0: if(x)
+NEXT_STATE=S1;
+else
+NEXT_STATE=S0;
+S1: if(x)
+NEXT_STATE=S1;
+else
+NEXT_STATE=S2;
+S2: if(x)
+NEXT_STATE=S3;
+else
+NEXT_STATE=S0;
+S3: if(x)
+NEXT_STATE=S1;
+else
+NEXT_STATE=S2;
 endcase
-//define the sequential block
 always@(negedge rst, posedge clk)
-
-
-
-assign z=(present_state==S3); // define output
+if(rst)
+present_state<=S0;
+else
+present_state<=NEXT_STATE;
+assign z=(present_state==S3);
 endmodule
